599f348188141d4dce7071a2d110ec90b3f5224c
Sujanamar <amarsujana04@gmail.com>
2025-08-05 14:13:52 +0530

    changed a word

--- palindrome.c
+++ palindrome.c
@@ -20,7 +20,9 @@ int main() {
    if (isPalindrome)
         printf("'%s' is a palindrome.\n", string);
     else
-        printf("'%s' is not a palindrome.\n", string);
+        printf("'%s' isn't a palindrome.\n", string);
       return 0;
 }
-  this is  if  branch.... 
+
+
+this is  if  branch.... 
